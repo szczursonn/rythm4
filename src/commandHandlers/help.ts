e14444dbafd62da6ebd9d5ddca025b0494533b6b
Michał Szczurek <michalszczurek794@gmail.com>
2021-09-22 01:12:58 +0200

    convert all strings to template strings

--- src/commandHandlers/help.ts
+++ src/commandHandlers/help.ts
@@ -1,22 +1,22 @@
 import { MessageEmbed, MessageOptions } from "discord.js"
 
-export const helpHandler = async (prefix: string, reply: (msg: MessageOptions | string)=>any) => {
+export const helpHandler = async (PREFIX: string, reply: (msg: MessageOptions | string)=>any) => {
     const embed = new MessageEmbed()
             .setTitle(`List of commands`)
             .setColor('#0189df')
             .setURL('https://github.com/szczursonn/Rythm4/blob/master/Rythm4/README.md')
-                .setDescription('Prefix: **' + prefix + '**')
+                .setDescription('Prefix: **' + PREFIX + '**')
                 .addFields(
-                    { name: '\:play_pause: Play', value: '**' + prefix + 'play** lub **' + prefix + 'p**', inline: true },
-                    { name: '\:track_next: Skip', value: '**' + prefix + 'skip** lub **' + prefix + 's**', inline: true },
-                    { name: '\:page_facing_up: Queue', value: '**' + prefix + 'queue** lub **' + prefix + 'q**', inline: true },
-                    { name: '\:loudspeaker: Volume', value: '**' + prefix + 'volume** lub **' + prefix + 'vol** lub **' + prefix + 'v**', inline: true },
-                    { name: '\:pause_button: Pause', value: '**' + prefix + 'pause**', inline: true },
-                    { name: '\:play_pause: Unpause', value: '**' + prefix + 'unpause** lub **' + prefix + 'resume**', inline: true },
-                    { name: '\:stop_sign: Disconnect', value: '**' + prefix + 'disconnect** lub **' + prefix + 'dc** lub **' + prefix + 'fuckoff**', inline: true },
-                    { name: '\:arrows_counterclockwise: Loop', value: '**' + prefix + 'loop**', inline: true },
-                    { name: ':cyclone: Shuffle', value: `**${prefix}shuffle**`, inline: true},
-                    { name: '\:question: Help', value: '**' + prefix + 'help**', inline: true },
+                    { name: '\:play_pause: Play', value: `**${PREFIX}play** or **${PREFIX}p**`, inline: true },
+                    { name: '\:track_next: Skip', value: `**${PREFIX}skip** or **${PREFIX}s**`, inline: true },
+                    { name: '\:page_facing_up: Queue', value: `**${PREFIX}queue** or **${PREFIX}q**`, inline: true },
+                    { name: '\:loudspeaker: Volume', value: `**${PREFIX}volume** or **${PREFIX}vol**`, inline: true },
+                    { name: '\:pause_button: Pause', value: `**${PREFIX}pause**`, inline: true },
+                    { name: '\:play_pause: Unpause', value: `**${PREFIX}unpause** or **${PREFIX}resume**`, inline: true },
+                    { name: '\:stop_sign: Disconnect', value: `**${PREFIX}disconnect** or **${PREFIX}dc** or **${PREFIX}fuckoff**`, inline: true },
+                    { name: '\:arrows_counterclockwise: Loop', value: `**${PREFIX}loop**`, inline: true },
+                    { name: ':cyclone: Shuffle', value: `**${PREFIX}shuffle**`, inline: true},
+                    { name: '\:question: Help', value: `**${PREFIX}help**`, inline: true },
                 )
 
     reply({embeds: [embed]})
