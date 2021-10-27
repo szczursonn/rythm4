1af8c4d1e5660fbc6a4a7e38338a5f2d81de8685
Michał Szczurek <michalszczurek794@gmail.com>
2021-10-27 18:38:43 +0200

    export node enviroment

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,4 @@ config()
 
 export const PREFIX = process.env.PREFIX || '%'
 export const DISCORD_TOKEN = process.env.DISCORD_TOKEN
+export const IS_DEVELOPMENT = process.env.NODE_ENV !== 'production'
