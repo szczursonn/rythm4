77e6ef1ce37600574cb0e8977938b13371582f46
Michał Szczurek <michalszczurek794@gmail.com>
2021-10-28 13:16:39 +0200

    export current git commit id

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,14 @@
 import { config } from "dotenv"
+import { execSync } from 'child_process'
 config()
+
+
+
+try {
+    commitId = execSync('git rev-parse HEAD').toString().trim().substr(0, 7)
+} catch (e) {}
 
 export const PREFIX = process.env.PREFIX || '%'
 export const DISCORD_TOKEN = process.env.DISCORD_TOKEN
 export const IS_DEVELOPMENT = process.env.NODE_ENV !== 'production'
+export var commitId: string | undefined
