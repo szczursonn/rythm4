ca5726aef4fd298611011dd77630a8580c9753dd
Michał Szczurek <michalszczurek794@gmail.com>
2021-12-11 19:05:24 +0100

    color log labels with chalk

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,4 +1,5 @@
 import { NODE_ENV } from "../config"
+import chalk from "chalk"
 
 export enum LoggingLabel {
     INFO = 'INFO',
@@ -7,8 +8,26 @@ export enum LoggingLabel {
     DEBUG = 'DEBUG'
 }
 
+const formatLabel = (label: LoggingLabel): string => {
+    switch (label) {
+        case LoggingLabel.INFO:
+            return chalk.green(label)
+        case LoggingLabel.ERROR:
+            return chalk.red(label)
+        case LoggingLabel.WARNING:
+            return chalk.yellow(label)
+        case LoggingLabel.DEBUG:
+            return chalk.gray(label)
+        default:
+            return chalk.strikethrough(label)
+    }
+}
+
 export const log = (message: any, label: LoggingLabel): void => {
     if (label === LoggingLabel.DEBUG && NODE_ENV !== 'development') return
-    const formattedMessage = `${new Date().toLocaleString()} | ${label} | ${message}`
+
+    const formattedLabel = formatLabel(label)
+
+    const formattedMessage = `${new Date().toLocaleString()} | ${formattedLabel} | ${message}`
     console.log(formattedMessage)
 }
