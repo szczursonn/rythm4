36245e6c4a486e98c908fa58b439ae7644c6f40a
Michał Szczurek <michalszczurek794@gmail.com>
2021-12-11 16:14:48 +0100

    export NODE_ENV

--- src/config.ts
+++ src/config.ts
@@ -11,4 +11,5 @@ try {
 export const PREFIX = process.env.PREFIX || '%'
 export const DISCORD_TOKEN = process.env.DISCORD_TOKEN
 export const IS_DEVELOPMENT = process.env.NODE_ENV !== 'production'
+export const NODE_ENV = process.env.NODE_ENV || 'development'
 export var commitId: string | undefined
