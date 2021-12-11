0a1e9ddbc72fb36f2e45e3abeb9abb82f88c5ae3
Michał Szczurek <michalszczurek794@gmail.com>
2021-12-11 17:53:22 +0100

    install @types/usetube

--- src/commandHandlers/play.ts
+++ src/commandHandlers/play.ts
@@ -1,7 +1,7 @@
 import { Permissions, StageChannel, VoiceChannel } from "discord.js";
 import { DiscordGatewayAdapterCreator, entersState, joinVoiceChannel, VoiceConnectionStatus } from "@discordjs/voice";
 import ytfps from 'ytfps';
-const usetube = require('usetube')
+import usetube from 'usetube';
 
 import Session from "../Session";
 import Song from "../Song";
